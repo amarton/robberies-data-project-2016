741de140ba4f0e3ecfc733e094d849a5eb218c2e
Adam Marton <adam@adammarton.com>
2016-12-20 17:12:51 -0500

    final!

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -2,7 +2,7 @@ var app = app || {
 
 	init: function(){
 
-		//app.share();
+		app.share();
 		app.show_video();
 
 	},
@@ -19,8 +19,8 @@ var app = app || {
 
 		$(".icon-twitter").on("click", function(){
 
-			var tweet = "A new wave of refugees, often from far-flung countries, are landing at Patterson High School. Read their stories: "; //Tweet text
-			var url = "http://data.baltimoresun.com/news/unsettled-journeys/part-1/"; //Interactive URL
+			var tweet = "Examining Baltimore's 2016 increase in robberies"; //Tweet text
+			var url = "http://data.baltimoresun.com/news/robberies2016/"; //Interactive URL
 
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
 			window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
@@ -29,10 +29,10 @@ var app = app || {
 
 		$(".icon-facebook").on("click", function(){
 
-			var picture = "http://data.baltimoresun.com/news/unsettled-journeys/part-1/images/topper.jpg"; //Picture URL
-			var title = "Unsettled Journeys: Torn between two worlds"; //Post title
-			var description = "A new wave of refugees, often from far-flung countries, are landing at Patterson High School. Read their stories."; //Post description
-			var url = "http://data.baltimoresun.com/news/unsettled-journeys/part-1/"; //Interactive URL
+			var picture = "http://data.baltimoresun.com/news/robberies2016/dist/images/facebook-thumb.png"; //Picture URL
+			var title = "Baltimore Robberies"; //Post title
+			var description = "Examining Baltimore's 2016 increase in robberies."; //Post description
+			var url = "http://data.baltimoresun.com/news/robberies2016/"; //Interactive URL
 
 	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
